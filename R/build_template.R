63c2d9ad88a8fd53fcea430121b66c9ba6ba0738
Yulia Sidi <yulia.sidi@uconn.edu>
2018-08-09 22:08:04 -0400

    first commit

---
+++ R/build_template.R
@@ -0,0 +1,28 @@
+build_template <- function(
+  file,
+  transfer = 'YES',
+  transfer_time = 'ON_EXIT',
+  args = '$(Process)',
+  tag = 'job',
+  init_dir = 'jobs',
+  input_files = NULL,
+  output_files = NULL,
+  jobs = 1
+){
+  this_template <- whisker::whisker.render(
+    template = readLines('~/Desktop/Rtips/template.condor'),
+    data = list(
+      file = file,
+      transfer = transfer,
+      transfer_time = transfer_time,
+      args = args,
+      tag=tag,
+      init_dir = init_dir,
+      input_files = input_files,
+      output_files = output_files,
+      jobs = jobs
+    )
+  )
+
+  cat(this_template)
+}
